bb91c4906777d7f48d55d5a28f56a675e6194af7
Filip Kirin <filip.kirin.zg1@gmail.com>
2023-10-02 19:56:26 +0200

    add variables

---
+++ variables/target/debug/deps/variables-b98b40ee63808534.d
@@ -0,0 +1,5 @@
+/home/kirin/Rust-Programs/variables/target/debug/deps/variables-b98b40ee63808534: src/main.rs
+
+/home/kirin/Rust-Programs/variables/target/debug/deps/variables-b98b40ee63808534.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/kirin/Rust-Programs/variables/target/debug/variables: /home/kirin/Rust-Programs/variables/src/main.rs
