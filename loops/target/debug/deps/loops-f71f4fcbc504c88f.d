9fc083085ef6ce70285788d61268c92431df6dec
Filip Kirin <filip.kirin.zg1@gmail.com>
2023-10-05 20:44:05 +0200

    add loops

---
+++ loops/target/debug/deps/loops-f71f4fcbc504c88f.d
@@ -0,0 +1,5 @@
+/home/kirin/Rust-Programs/loops/target/debug/deps/loops-f71f4fcbc504c88f: src/main.rs
+
+/home/kirin/Rust-Programs/loops/target/debug/deps/loops-f71f4fcbc504c88f.d: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/home/kirin/Rust-Programs/loops/target/debug/loops: /home/kirin/Rust-Programs/loops/src/main.rs
