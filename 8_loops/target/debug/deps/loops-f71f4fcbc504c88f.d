e0b4717a91ccaeeefa464502c1759436a6448255
Filip Kirin <filip.kirin.zg1@gmail.com>
2024-03-17 13:27:59 +0100

    added num in name

---
+++ 8_loops/target/debug/deps/loops-f71f4fcbc504c88f.d
@@ -0,0 +1,5 @@
+/home/kirin/Rust-Programs/8_loops/target/debug/deps/loops-f71f4fcbc504c88f: src/main.rs
+
+/home/kirin/Rust-Programs/8_loops/target/debug/deps/loops-f71f4fcbc504c88f.d: src/main.rs
+
+src/main.rs:
---
+++ 8_loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/home/kirin/Rust-Programs/8_loops/target/debug/loops: /home/kirin/Rust-Programs/8_loops/src/main.rs
