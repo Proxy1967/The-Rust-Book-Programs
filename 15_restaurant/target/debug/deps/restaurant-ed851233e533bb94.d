b70eb624439e1097ab94fb59128188c5f0f54e98
Filip Kirin <filip.kirin.zg1@gmail.com>
2024-06-20 20:23:13 +0200

    packages, crates and modules

---
+++ 15_restaurant/target/debug/deps/restaurant-ed851233e533bb94.d
@@ -0,0 +1,7 @@
+/home/kirin/The-Rust-Book-Programs/15_restaurant/target/debug/deps/librestaurant-ed851233e533bb94.rmeta: src/lib.rs
+
+/home/kirin/The-Rust-Book-Programs/15_restaurant/target/debug/deps/librestaurant-ed851233e533bb94.rlib: src/lib.rs
+
+/home/kirin/The-Rust-Book-Programs/15_restaurant/target/debug/deps/restaurant-ed851233e533bb94.d: src/lib.rs
+
+src/lib.rs:
---
+++ 15_restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/home/kirin/The-Rust-Book-Programs/15_restaurant/target/debug/librestaurant.rlib: /home/kirin/The-Rust-Book-Programs/15_restaurant/src/lib.rs
